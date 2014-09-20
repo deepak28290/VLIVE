c759862404d8c3376ff60bc8213eae7549f4d155
unknown <demalhotra@.corp.ebay.com>
2014-09-20 20:06:53 +0530

    modified insert/update logic for upstream logic and added route details table

--- svcProject/src/a/service/proj/ReturnCalls.java
+++ svcProject/src/a/service/proj/ReturnCalls.java
@@ -50,11 +50,11 @@ public class ReturnCalls {
 	  @Path("/firstUser")
 	  @Produces(MediaType.APPLICATION_JSON)
 	  @Consumes(MediaType.APPLICATION_JSON)
-	  public String crtUserData(@QueryParam(value = "userid") String userid,@QueryParam(value = "boardingpoint") String boardingpoint,@QueryParam(value = "droppoint") String droppoint,@QueryParam(value = "busnumb") String busnumb) throws ClientProtocolException, IOException {
-		String boord_coord;
-		String drop_coord;
-		boord_coord=VolvoUtil.getCoordFromLoc(boardingpoint);
-		drop_coord=VolvoUtil.getCoordFromLoc(droppoint);
+	  public String crtUserData(@QueryParam(value = "userid") String userid,@QueryParam(value = "boardingpoint") String boardingpoint,@QueryParam(value = "droppoint") String droppoint,@QueryParam(value = "busnumb") String busnumb,@QueryParam(value = "currentcoord") String currentcoord) throws ClientProtocolException, IOException {
+		String boardcoord;
+		String dropcoord;
+		boardcoord=VolvoUtil.getCoordFromLoc(boardingpoint);
+		dropcoord=VolvoUtil.getCoordFromLoc(droppoint);
 		
 		 Connection connection = null;
 			Statement stmt = null;
@@ -71,10 +71,11 @@ public class ReturnCalls {
 				connection = DriverManager.getConnection(url, "deepak111", "deepak");
 
 				stmt = connection.createStatement();
-
+				String up=VolvoUtil.isUpStream(boardingpoint, droppoint);
 				System.out.println(sql);
-				String getSql = "INSERT INTO volvo_table VALUES('deepakId','Marathalli','Kadugodi','100,100','120,120','111,111','22E','0','','')";
-				//String getSql2 = "INSERT INTO volvo_table VALUES('"+userid+"','"+boardingpoint+"','"+droppoint+"','100,100','120,120','"+busnumb+"','0',null)";
+				//String getSql = "INSERT INTO volvo_table VALUES('deepakId','Marathalli','Kadugodi','100,100','120,120','111,111','22E','0','','')";
+				String getSql = "INSERT INTO volvo_table VALUES('"+userid+"','"+boardingpoint+"','"+droppoint+"','"+boardcoord+"','"+dropcoord+"','"+currentcoord+"','"+busnumb+"','0',null,'"+up+"')";
+				System.out.println(getSql);
 				stmt.executeUpdate(getSql);
 				// STEP 5: Extract data from result set
 //				System.out.println(memcached.get(""));
@@ -109,7 +110,7 @@ public class ReturnCalls {
 					connection = DriverManager.getConnection(url, "deepak111", "deepak");
 
 					stmt = connection.createStatement();
-
+					
 					System.out.println(sql);
 					String getSql = "UPDATE volvo_table SET current_coord='"+coord+"' WHERE user_id='"+userid+"'";
 					//String getSql2 = "INSERT INTO volvo_table VALUES('"+userid+"','"+boardingpoint+"','"+droppoint+"','100,100','120,120','"+busnumb+"','0',null)";
--- svcProject/src/a/service/proj/VolvoUtil.java
+++ svcProject/src/a/service/proj/VolvoUtil.java
@@ -7,7 +7,60 @@ import java.sql.Statement;
 
 public class VolvoUtil {
 
+	public static String isUpStream(String loc1,String loc2){
+		
+		 Connection connection = null;
+			Statement stmt = null;
+			String lat="";
+			String longt="";
+			String coord;
+			String sql = new String();
+			String s1="";
+			String s2="";
+		 try {
+				// the sql server driver string
+				Class.forName("com.mysql.jdbc.Driver");
+
+				// the sql server url
+				String url = "jdbc:mysql://db4free.net:3306/volvolive";
+				
+				// get the sql server database connection
+				connection = DriverManager.getConnection(url, "deepak111", "deepak");
+
+				stmt = connection.createStatement();
+
+				System.out.println(sql);
+				String getSql = "SELECT * from volvo_bus_routes WHERE busstop_name='"+loc1+"'";
+				//String getSql2 = "INSERT INTO volvo_table VALUES('"+userid+"','"+boardingpoint+"','"+droppoint+"','100,100','120,120','"+busnumb+"','0',null)";
+			ResultSet rs=	stmt.executeQuery(getSql);
+			if (rs.next()) {
+			s1=rs.getString("Serial Number");
+		
+			}
+			 getSql = "SELECT * from volvo_bus_routes WHERE busstop_name='"+loc2+"'";
+			//String getSql2 = "INSERT INTO volvo_table VALUES('"+userid+"','"+boardingpoint+"','"+droppoint+"','100,100','120,120','"+busnumb+"','0',null)";
+		rs=	stmt.executeQuery(getSql);
+		if (rs.next()) {
+		s2=rs.getString("Serial Number");
 	
+		}
+		
+		if(Integer.parseInt(s1)-Integer.parseInt(s2)>0){
+			System.out.println(Integer.parseInt(s1)-Integer.parseInt(s2));
+			return "1";
+		}else{
+			return "0";
+		}
+				// STEP 5: Extract data from result set
+//				System.out.println(memcached.get(""));
+			
+						
+					}catch(Exception e){
+						e.printStackTrace();
+						return "1";
+					}
+	
+	}
 	public static String getCoordFromLoc(String loc){
 		 Connection connection = null;
 			Statement stmt = null;
@@ -33,8 +86,13 @@ public class VolvoUtil {
 			ResultSet rs=	stmt.executeQuery(getSql);
 			if (rs.next()) {
 			lat=rs.getString("latitude");
+			lat.replaceAll("\r", "");
+			lat.replaceAll("\n", "");
 			longt=rs.getString("longitude");
-				
+			longt.replaceAll("\r", "");
+			longt.replaceAll("\n", "");
+			longt.trim();
+			lat.trim();
 			}
 				// STEP 5: Extract data from result set
 //				System.out.println(memcached.get(""));
